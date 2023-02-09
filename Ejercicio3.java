2b07cae0be9432eb06292929f6adfebc1c78e82a
pablocavaz04 <camvazpablo04@gmail.com>
2023-02-09 10:26:33 +0100

    First Commit

---
+++ Ejercicio3.java
@@ -0,0 +1,21 @@
+/*
+3) Escribe un programa que lea 10 números por teclado y que luego los muestre
+en orden inverso, es decir, el primero que se introduce es el último en mostrarse
+y viceversa.
+ */
+import java.util.Scanner;
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int [] numero = new int[10];
+
+        for(int i=0;i<10;i++){
+            System.out.println("Introduzca un numero: ");
+            numero [i] = sc.nextInt();
+
+        }
+        for(int i=9;i>=0;i--){
+            System.out.println(numero[i]);
+        }
+    }
+}
